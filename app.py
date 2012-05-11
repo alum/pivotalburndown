6910d320cae42a5c5cc9068f90ce82f25b637f7f
Filip <filip.bonnevier@gmail.com>
2012-05-11 17:43:35 +0200

    testing...

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 import os
 import pivotal
 import settings
-
+from busyflow.pivotal import *
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-	data = { 'test':'test', 'apa':'apan' }
+	client = PivotalClient(token=settings.token, cache='')
+	#projects = client.projects.all()['projects']
+	data = client.iterations.current(settings.project_id)
+	point_sum = sum(map(lambda x: 0 if 'estimate' not in x else x['estimate'], data['iterations'][0]['stories']))
+	#print apa
+
+	# find max and min dates for accepted stories
+	for iteration in data['iterations']:
+		dates = []
+		for story in iteration['stories']:
+			if 'accepted_at' in story:
+				dates.append(story['accepted_at'])
+		iteration['max_date'] = max(dates)
+		iteration['min_date'] = min(dates)
+
+	
+
+	for iteration in data['iterations']:
+		for story in iteration['stories']:
+			if 'estimate' not in story:
+				story['estimate'] = 0
+			if story['estimate'] == 'accepted':
+				point_sum -= story['estimate']
+				story['burndown'] = point_sum
+	data = { 'iterations':data['iterations']}
 	return render_template('main.html', data=data)
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 # Fill out your project settings and API token here!
-project_id = '365149'
+#project_id = '365149'
+project_id = '188547'
 token = '4bf609979b1fc8f13a41f98df4f6c6aa'
