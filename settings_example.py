d00937235b29d4f723cb83d6cdbcb3d2dea841ac
Filip <filip.bonnevier@gmail.com>
2012-05-14 09:12:14 +0200

    fix

---
+++ settings_example.py
@@ -0,0 +1,6 @@
+# Fill out your project settings and API token here!
+
+# get your project ID from the URL to the project, e.g. https://www.pivotaltracker.com/projects/1234567
+project_id = ''
+# get your token: curl -u $USERNAME:$PASSWORD -X GET https://www.pivotaltracker.com/services/v3/tokens/active
+token = ''
