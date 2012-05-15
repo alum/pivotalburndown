16538ac3477146727d19efe037c03fcf1d5358c9
Filip <filip.bonnevier@gmail.com>
2012-05-15 16:16:16 +0200

    auth

--- settings_example.py
+++ settings_example.py
@@ -1,4 +1,5 @@
-# Fill out your project settings and API token here!
+# Fill out your API token and desired user/pass combo here!
+# Rename file to settings.py when done
 # get your token: curl -u $USERNAME:$PASSWORD -X GET https://www.pivotaltracker.com/services/v3/tokens/active
 token = ''
 username = ''
